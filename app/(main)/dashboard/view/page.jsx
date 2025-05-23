fa9354c625a9b72adcffbc04036a8a63edd85bbf
Bernards Widiyazulfathirrochim <bernardswidiyazulfathirrochim@Bernards-MacBook-Pro.local>
2025-05-23 11:51:51 +0700

    view update

--- app/(main)/dashboard/view/page.jsx
+++ app/(main)/dashboard/view/page.jsx
@@ -3,6 +3,8 @@ import { ambilDataIjalBerau } from "@/lib/query/ijalberau";
 import { ambilNotes } from "@/lib/query/notes";
 import DefaultLayout from "@/components/DefaultLayout";
 import Image from "next/image";
+import ButtonHome from "@/components/ButtonHome";
+import ButtonContributtor from "@/components/ButtonContributtor";
 
 const Notes = ({ id, isi }) => {
   return (
@@ -21,5 +23,28 @@ export default async function Home() {
   // const data = await ambilNotes("isi");
   const { data, error } = await supabase.from("notes").select("*");
   console.log("data", data);
-  return <DefaultLayout className={"text-[#000000]"}>ADkasdaadj</DefaultLayout>;
+  return (
+    <>
+    <div className="flex w-full h-full items-center justify-center flex-col text-black pt-10">
+      <div className="flex flex-col w-[90%] h-[85%] bg-[red] text-[white]">
+        {/* atas */}
+        <div className="flex w-full h-[8%] bg-[green] text-[white] items-center">
+          <div className="w-[50%]"><ButtonHome></ButtonHome></div>
+          <div className="flex w-[50%] items-end bg-[blue] text-[white]">
+            <div></div>
+            <div><ButtonContributtor>Contributtor</ButtonContributtor></div>
+          </div>
+        </div>
+        {/* bawah */}
+        <div className ="flex w-full h-[92%] bg-[orange] text-[white]">
+          {/* Judul dkk */}
+          <div className="flex w-full h-[18%] bg-[blue]"></div>
+          {/* Isi */}
+          <div></div>
+        </div>
+      </div>
+      
+    </div>
+    </>
+  )
 }
---
+++ components/HeaderDocuments.js
@@ -0,0 +1,73 @@
+import PropTypes from "prop-types";
+import { cn } from "@/lib/utils";
+import { Info, SquarePen, Trash2 } from "lucide-react";
+
+export default function HeaderDocuments({ title, createdAt, username, Contributor, ...props }) {
+  return (
+    <div
+      className={cn(
+        "flex w-full items-center justify-between rounded-[8px] bg-white px-5 py-4 duration-300 ease-in-out hover:shadow-lg md:px-8 md:py-6",
+        className,
+      )}
+      {...props}
+    >
+      <div className="flex flex-col gap-[4px]">
+        <h1 className="font-eudoxus-bold text-start text-2xl text-[#16223B] duration-300 ease-in-out sm:text-3xl md:text-4xl">
+          {title}
+        </h1>
+        <div className="item-start g ap-[4px] flex flex-col duration-300 ease-in-out md:flex-row md:items-center md:gap-[16px]">
+          <p className="font-eudoxus-medium text-[12px] text-[#B89347] duration-300 ease-in-out sm:text-sm">
+            {createdAt}
+          </p>
+          <p className="font-eudoxus-bold text-[12px] text-[#16223B] duration-300 ease-in-out sm:text-sm">
+            {viewDoc}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-2 duration-300 ease-in-out md:flex-row md:gap-[20px]">
+        <button
+          type="button"
+          className="flex cursor-pointer items-center gap-[8px] rounded-full bg-[#5DF590] p-2 text-xl text-white duration-300 ease-in-out hover:bg-[#4FD07B] md:rounded-[5px] md:px-4 md:py-2.5"
+        >
+          <span className="font-eudoxus-medium hidden text-sm text-[#16223B] duration-300 ease-in-out md:inline">
+            Details
+          </span>
+          <Info
+            className="h-2.5 w-2.5 text-[#16223B] duration-300 ease-in-out sm:h-4 sm:w-4"
+            strokeWidth={2}
+          />
+        </button>
+        <button
+          type="button"
+          className="flex cursor-pointer items-center gap-[8px] rounded-full bg-[#5DCCF5] p-2 text-xl text-white duration-300 ease-in-out hover:bg-[#4FAED0] md:rounded-[5px] md:px-4 md:py-2.5"
+        >
+          <span className="font-eudoxus-medium hidden text-sm text-[#16223B] duration-300 ease-in-out md:inline">
+            Edit
+          </span>
+          <SquarePen
+            className="h-2.5 w-2.5 text-[#16223B] duration-300 ease-in-out sm:h-4 sm:w-4"
+            strokeWidth={2}
+          />
+        </button>
+        <button
+          type="button"
+          className="flex cursor-pointer items-center gap-[8px] rounded-full bg-[#F55D60] p-2 text-xl text-white duration-300 ease-in-out hover:bg-[#D04F52] md:rounded-[5px] md:px-4 md:py-2.5"
+        >
+          <span className="font-eudoxus-medium hidden text-sm text-[#16223B] duration-300 ease-in-out md:inline">
+            Hapus
+          </span>
+          <Trash2
+            className="h-2.5 w-2.5 text-[#16223B] duration-300 ease-in-out sm:h-4 sm:w-4"
+            strokeWidth={2}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
+DashboardItem.propTypes = {
+  viewDoc: PropTypes.isRequired,
+  createdAt: PropTypes.isRequired,
+  title: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
